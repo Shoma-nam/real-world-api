7de6370b5c8f4135db850ca338c756df71a0e943
shome <yubemto12@gmail.com>
2024-01-07 15:43:54 +0900

    tagList-logic

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,16 +4,28 @@ class ArticlesController < ApplicationController
         render json: articles
     end
     def create
-        article = Article.new(article_params)
-        if article.save
-            render json: { article: article }, status: :created
-        else
-            render json: { errors: article.errors }, status: :unprocessable_entity
-        end
+      article = Article.new(article_params.except(:tagList))
+      if article.save
+        handle_tag_list(article, article_params[:tagList])
+        render json: { article: article }, status: :created
+      else
+        render json: { errors: article.errors }, status: :unprocessable_entity
+      end
     end
+  
     private
+  
     def article_params
-        params.require(:article).permit(:title, :description, :body, tagList: [])
+      params.require(:article).permit(:title, :description, :body, tagList: [])
     end
+  
+    def handle_tag_list(article, tag_list)
+      return if tag_list.blank?
+  
+      tag_list.each do |tag_name|
+        tag = Tag.find_or_create_by(name: tag_name)
+        article.tags << tag unless article.tags.include?(tag)
+      end
     end
 end
+
